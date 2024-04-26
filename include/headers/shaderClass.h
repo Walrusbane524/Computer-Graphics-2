6414e8af1e6c386ac1e2db0e80319523b281cdd6
Murilo <murilovpinheiro@gmail.com>
2024-04-26 17:30:14 -0300

    Começando com separação das classes shaders

---
+++ include/headers/shaderClass.h
@@ -0,0 +1,31 @@
+//
+// Created by Murilo on 26/04/2024.
+//
+
+#ifndef CG2_SHADERCLASS_H
+#define CG2_SHADERCLASS_H
+
+#include <glad/glad.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <cerrno>
+
+using namespace std;
+
+string get_file_contents(const char* filename);
+
+class ShaderClass{
+
+public:
+    GLuint id;
+
+    ShaderClass(const char* vertexFile, const char* fragmentFile);
+
+    void Activate();
+    void Delete();
+};
+
+
+#endif //CG2_SHADERCLASS_H
---
+++ src/classes/shaderClass.cpp
@@ -0,0 +1,70 @@
+//
+// Created by Murilo on 26/04/2024.
+//
+#include "../../include/headers/shaderClass.h"
+
+string get_file_contents(const char* filename)
+{
+    ifstream in(filename, ios::binary);
+    if (in)
+    {
+        string contents;
+        in.seekg(0, ios::end);
+        contents.resize(in.tellg());
+        in.seekg(0, ios::beg);
+        in.read(&contents[0], contents.size());
+        in.close();
+        return(contents);
+    }
+    throw(errno);
+}
+
+ShaderClass::ShaderClass(const char* vertexFile, const char* fragmentFile)
+{
+    // Read vertexFile and fragmentFile and store the strings
+    string vertexCode = get_file_contents(vertexFile);
+    string fragmentCode = get_file_contents(fragmentFile);
+
+    // Convert the shader source strings into character arrays
+    const char* vertexSource = vertexCode.c_str();
+    const char* fragmentSource = fragmentCode.c_str();
+
+    // Create Vertex Shader Object and get its reference
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    // Attach Vertex Shader source to the Vertex Shader Object
+    glShaderSource(vertexShader, 1, &vertexSource, NULL);
+    // Compile the Vertex Shader into machine code
+    glCompileShader(vertexShader);
+
+    // Create Fragment Shader Object and get its reference
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    // Attach Fragment Shader source to the Fragment Shader Object
+    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+    // Compile the Vertex Shader into machine code
+    glCompileShader(fragmentShader);
+
+    // Create Shader Program Object and get its reference
+    id = glCreateProgram();
+    // Attach the Vertex and Fragment Shaders to the Shader Program
+    glAttachShader(id, vertexShader);
+    glAttachShader(id, fragmentShader);
+    // Wrap-up/Link all the shaders together into the Shader Program
+    glLinkProgram(id);
+
+    // Delete the now useless Vertex and Fragment Shader objects
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+}
+
+// Activates the Shader Program
+void ShaderClass::Activate()
+{
+    glUseProgram(id);
+}
+
+// Deletes the Shader Program
+void ShaderClass::Delete()
+{
+    glDeleteProgram(id);
+}
